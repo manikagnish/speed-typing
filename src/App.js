1a73c63c36dc8f1e3cb4a979adc8279486851a9f
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-11-07 17:05:00 +0530

    implemented input text and word count functionality

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import { useState, useEffect, useRef } from 'react';
 
 function App() {
   const START_TIME = 5;
+  const [text, setText] = useState('');
   const [timeRemaining, setTimeRemaining] = useState(START_TIME);
   const [wordCount, setWordCount] = useState(0);
   const [start, setStart] = useState(false);
   const [playAgain, setPlayAgain] = useState('Start');
-  let played = false;
+
+  const inputRef = useRef(null);
 
   function startGame() {
+    setText('');
     setStart(true);
+    setTimeout(() => {
+      inputRef.current.focus();
+    }, 100);
   }
 
   function endGame() {
     setStart(false);
     setPlayAgain('Play Again');
     setTimeRemaining(START_TIME);
+    let totalWords = text.trim().split(' ').length;
+    setWordCount(totalWords);
   }
 
   useEffect(() => {
@@ -31,7 +39,12 @@ function App() {
   return (
     <div>
       <h1>Test your typing speed!</h1>
-      <textarea></textarea>
+      <textarea
+        disabled={!start}
+        ref={inputRef}
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
       <p>Time remaining: {timeRemaining}</p>
       <button onClick={startGame} disabled={start}>
         {playAgain}
