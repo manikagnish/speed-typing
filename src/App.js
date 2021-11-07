d8f5a3a05ec378d5ca9a000b14631f2a88e40171
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-11-07 15:41:05 +0530

    added styling to the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div>hello world!</div>;
+  return (
+    <div>
+      <h1>Test your typing speed!</h1>
+      <textarea></textarea>
+      <p>Time remaining: 0</p>
+      <button>Start</button>
+      <p>Your typing speed is: 0 wpm</p>
+    </div>
+  );
 }
 
 export default App;
