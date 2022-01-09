a27f63f6e0f470a639396b03eabec76e4df637fb
manikagnish <72827130+manikagnish@users.noreply.github.com>
2022-01-09 19:41:34 +0530

    created mvp

--- src/useSpeedType.js
+++ src/useSpeedType.js
@@ -1,109 +1,63 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from "react";
 
 export default function useSpeedType(gameTime = 60) {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [timeRemaining, setTimeRemaining] = useState(gameTime);
   const [wordCount, setWordCount] = useState(0);
   const [start, setStart] = useState(false);
-  const [playAgain, setPlayAgain] = useState('Start');
-  const [quote, setQuote] = useState('');
+  const [playAgain, setPlayAgain] = useState("Start");
+  const [quote, setQuote] = useState(
+    "The quick brown fox jumps over the lazy dog."
+  );
+  const [quoteArr, setQuoteArr] = useState("");
+  const [textArr, setTextArr] = useState("");
+  let words = 0;
 
-  const quoteLine = 'The quick brown fox jumps over the lazy dog.';
   const inputRef = useRef(null);
-  const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random';
 
   function startGame() {
-    setText('');
+    setText("");
     setStart(true);
     setTimeout(() => {
       inputRef.current.focus();
     }, 100);
   }
 
+  const splitText = (para, setPara) => {
+    setPara(para.split(" "));
+  };
+
+  useEffect(() => {
+    splitText(quote, setQuoteArr);
+  }, [quote]);
+
+  useEffect(() => {
+    text !== "" && splitText(text, setTextArr);
+  }, [text]);
+
   function endGame() {
     setStart(false);
-    setPlayAgain('Play Again');
+    setPlayAgain("Play Again");
     setTimeRemaining(gameTime);
-    let totalWords = text.trim().split(' ').length;
-    setWordCount(totalWords);
-  }
 
-  // function getRandomQuote() {
-  //   return fetch(RANDOM_QUOTE_API_URL)
-  //     .then(response => response.json())
-  //     .then(data => data.content);
-  // }
-
-  // async function renderNewQuote() {
-  //   const quoted = await getRandomQuote();
-  //   const dividedQuote = quoted.split('').map(qt => <span>{qt}</span>);
-  //   setQuote(dividedQuote);
-  //   const arrayQuote = [];
-  //   for (let i = 0; i < dividedQuote.length; i++) {
-  //     arrayQuote.push(dividedQuote[i].props.children);
-  //   }
-  //   return arrayQuote;
-  // }
-
-  const arrayQuote = [];
-  function getQuote() {
-    arrayQuote.push(
-      quoteLine.split('').map(qt => <span className="correct">{qt}</span>)
-    );
-    console.log('arrayQuote: ', arrayQuote);
-    setQuote(arrayQuote);
-  }
-
-  function wordChecker() {
-    console.log('ran');
-    const arrayValue = text.split('');
-    console.log('arrayValue: ', arrayValue);
-
-    let correct = true;
-    arrayQuote.forEach((characterSpan, index) => {
-      console.log(characterSpan);
-      const character = arrayValue[index];
-      if (character == null) {
-        // characterSpan.classList.remove('correct');
-        // characterSpan.classList.remove('incorrect');
-        correct = false;
-        // console.log(correct);
-      } else if (character === characterSpan) {
-        // characterSpan.classList.add('correct');
-        // characterSpan.classList.remove('incorrect');
-        console.log(correct);
-      } else {
-        // characterSpan.classList.remove('correct');
-        // characterSpan.classList.add('incorrect');
-        correct = false;
-        // console.log(correct);
+    // compare quoteArr and textArr
+    for (let i = 0; i < quoteArr.length; i++) {
+      if (quoteArr[i] === textArr[i]) {
+        words++;
       }
-    });
-    // if (correct) renderNewQuote();
+    }
+    setWordCount(words);
   }
 
-  const firstUpdate = useRef(true);
   useEffect(() => {
     if (timeRemaining > 0 && start) {
       setTimeout(() => {
-        setTimeRemaining(time => time - 1);
+        setTimeRemaining((time) => time - 1);
       }, 1000);
     } else if (timeRemaining === 0) {
       endGame();
     }
   }, [timeRemaining, start]);
-
-  useEffect(() => {
-    getQuote();
-  }, []);
-
-  useEffect(() => {
-    if (firstUpdate.current) {
-      firstUpdate.current = false;
-      return;
-    }
-    wordChecker();
-  }, [text]);
 
   return [
     start,
